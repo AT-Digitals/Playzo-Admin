1faaab681fe7d178c8ed22f36feb386cb8133157
Antoshoba <antoshoba@gmail.com>
2023-12-20 21:37:46 +0530

    calendar list

--- src/pages/extra-pages/bookings/CalendarBooking.js
+++ src/pages/extra-pages/bookings/CalendarBooking.js
@@ -1,81 +1,14 @@
-import React, { useState, useEffect } from 'react';
-import { Calendar, momentLocalizer } from 'react-big-calendar';
-import moment from 'moment';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
-import MainCard from 'components/MainCard';
+
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import React, { useEffect, useState } from 'react';
+
 import BookingApi from 'api/BookingApi';
 import DateUtils from 'utils/DateUtils';
+import MainCard from 'components/MainCard';
+import moment from 'moment';
 
 const localizer = momentLocalizer(moment);
-
-const events = [
-  // {
-  //   title: 'Event 1',
-  //   start: new Date(2023, 11, 12, 10, 30, 0, 0),
-  //   end: new Date(2023, 11, 12, 12, 30, 0, 0)
-  // },
-  // {
-  //   title: 'Event 2',
-  //   start: new Date(2023, 11, 15, 18, 30, 0, 0),
-  //   end: new Date(2023, 11, 15, 22, 30, 0, 0)
-  // },
-  // {
-  //   title: 'Event 3',
-  //   start: new Date(2023, 11, 3, 2, 0, 0, 0),
-  //   end: new Date(2023, 11, 3, 4, 0, 0, 0)
-  // },
-  // {
-  //   title: 'Event 4',
-  //   start: new Date(2023, 11, 22, 2, 30, 0, 0),
-  //   end: new Date(2023, 11, 22, 4, 30, 0, 0)
-  // },
-  // {
-  //   title: 'Event 5',
-  //   start: new Date(2023, 11, 30, 8, 0, 0, 0),
-  //   end: new Date(2023, 11, 30, 12, 0, 0, 0)
-  // },
-  // {
-  //   title: 'Event 6',
-  //   start: new Date(2023, 11, 12, 8, 0, 0, 0),
-  //   end: new Date(2023, 11, 12, 10, 0, 0, 0)
-  // },
-  // {
-  //   title: 'Event 7',
-  //   start: new Date(2023, 11, 12, 14, 0, 0, 0),
-  //   end: new Date(2023, 11, 12, 17, 0, 0, 0)
-  // },
-  // {
-  //   title: 'Event 8',
-  //   start: new Date(2023, 11, 12, 18, 0, 0, 0),
-  //   end: new Date(2023, 11, 12, 19, 0, 0, 0)
-  // },
-  // {
-  //   title: 'Event 9',
-  //   start: new Date(2023, 11, 12, 19, 30, 0, 0),
-  //   end: new Date(2023, 11, 12, 22, 30, 0, 0)
-  // },
-  // {
-  //   title: 'Event 10',
-  //   start: new Date(2023, 11, 12, 1, 30, 0, 0),
-  //   end: new Date(2023, 11, 12, 2, 30, 0, 0)
-  // },
-  // {
-  //   title: 'Event 11',
-  //   start: new Date(2023, 11, 12, 3, 30, 0, 0),
-  //   end: new Date(2023, 11, 12, 6, 30, 0, 0)
-  // },
-  // {
-  //   title: 'Event 12',
-  //   start: new Date(2023, 11, 12, 7, 0, 0, 0),
-  //   end: new Date(2023, 11, 12, 8, 0, 0, 0)
-  // },
-  // {
-  //   title: 'Event 13',
-  //   start: new Date(2023, 11, 13, 1, 30, 0, 0),
-  //   end: new Date(2023, 11, 13, 2, 30, 0, 0)
-  // }
-];
-
 const CalendarBooking = () => {
   const [selectedEvent, setSelectedEvent] = useState(null);
   const [selectedDate, setSelectedDate] = useState(moment().toDate());
@@ -84,34 +17,45 @@ const CalendarBooking = () => {
   const handleSelectEvent = (event) => {
     setSelectedEvent(event);
   };
-  console.log('select', selectedEvent);
 
   const handleNavigate = (date) => {
     setSelectedDate(date);
   };
-  console.log('date', selectedDate);
-
-  // const formatMillisecondsToTime = (ms) => {
-  //   if (ms === null) {
-  //     return '';
-  //   }
-  //   const formattedTime = moment(ms).format('hh:mm:ss a');
-  //   return formattedTime;
-  // };
 
   useEffect(() => {
     const fetchInfo = async () => {
+      const listDate = [];
       try {
-        await BookingApi.getAll().then((data) => {
-          console.log('dateofbooking', data.dateOfBooking);
-          const day = DateUtils.formatDate(new Date(data.dateOfBooking), 'DD');
-          const month = DateUtils.formatDate(new Date(data.dateOfBooking), 'MM');
-          const year = DateUtils.formatDate(new Date(data.dateOfBooking), 'YYYY');
-          console.log('day', day, month, year);
-          setData(data);
+        await BookingApi.getAll().then((dataList) => {
+          dataList.map((list) => {
+            const dateObject = new Date(list.dateOfBooking);
+
+            const day = dateObject.getDate();
+            const month = DateUtils.formatDate(dateObject, 'MM');
+            const year = dateObject.getFullYear();
+
+            const startMilliseconds = parseInt(list.startTime);
+            const endMilliseconds = parseInt(list.endTime);
+
+            // const seconds = Math.floor((milliseconds / 1000) % 60);
+
+            const startMinutes = Math.floor((startMilliseconds / 1000 / 60) % 60);
+
+            const startHours = Math.floor((startMilliseconds / 1000 / 60 / 60) % 24);
+            const endMinutes = Math.floor((endMilliseconds / 1000 / 60) % 60);
+
+            const endHours = Math.floor((endMilliseconds / 1000 / 60 / 60) % 24);
+
+            const calendarDataList = {
+              title: list.type,
+              start: new Date(year, month, day, startHours.toString().padStart(2, '0'), startMinutes.toString().padStart(2, '0'), 0, 0),
+              end: new Date(year, month, day, endHours.toString().padStart(2, '0'), endMinutes.toString().padStart(2, '0'), 0, 0)
+            };
+            listDate.push(calendarDataList);
+          });
+
+          setData(listDate);
         });
-        // const details = await res.json();
-        // setData(details);
       } catch {
         console.log('vcxvcxv');
       }
@@ -119,38 +63,7 @@ const CalendarBooking = () => {
 
     fetchInfo();
   }, []);
-  console.log('data', data);
-
-  // const newArray = data.map((array) => {
-  //   const dateObject = new Date(array.dateOfBooking);
-  //   const year = dateObject.getFullYear();
-  //   const month = dateObject.getMonth() + 1;
-  //   const day = dateObject.getDate();
-  //   console.log(typeof year, typeof day, typeof month);
-
-  //   const value = new Date(year, month, day);
-  //   console.log('value', value);
-
-  // const startTime = formatMillisecondsToTime(array.startTime);
-  // const endTime = formatMillisecondsToTime(array.endTime);
-
-  // const momentStartObject = moment.utc(array.startTime);
-  // const hours = momentStartObject.hours();
-  // const minutes = momentStartObject.minutes();
-  // const seconds = momentStartObject.seconds();
-
-  // const momentEndObject = moment.utc(array.endTime);
-
-  // const ehours = momentEndObject.hours();
-  // const eminutes = momentEndObject.minutes();
-  // const eseconds = momentEndObject.seconds();
-
-  //   return {
-  //     title: array.type,
-  //     start: array.dateOfBooking,
-  //     end: array.dateOfBooking
-  //   };
-  // });
+  console.log('data', selectedEvent, selectedDate);
 
   return (
     <MainCard title="Calendar Booking">
@@ -159,7 +72,7 @@ const CalendarBooking = () => {
           <Calendar
             selectable
             localizer={localizer}
-            events={events}
+            events={data}
             startAccessor="start"
             endAccessor="end"
             onSelectEvent={handleSelectEvent}
--- src/utils/DateUtils.ts
+++ src/utils/DateUtils.ts
@@ -1,33 +1,33 @@
-import moment, { DurationInputArg1, unitOfTime } from "moment";
+import moment from 'moment';
 
 export default class DateUtils {
   static formatDate(date: Date | undefined, format: string) {
     if (!date) {
-      return "";
+      return '';
     }
     return moment(date).format(format);
   }
 
   static formatDateString(date: string, format: string) {
     if (!date) {
-      return "";
+      return '';
     }
     return DateUtils.formatDate(new Date(date), format);
   }
 
-  static add(
-    date: Date,
-    amount: DurationInputArg1,
-    value: unitOfTime.DurationConstructor
-  ) {
-    return moment(date).add(amount, value).toDate();
-  }
+  // static add(
+  //   date: Date,
+  //   amount: DurationInputArg1,
+  //   value: unitOfTime.DurationConstructor
+  // ) {
+  //   return moment(date).add(amount, value).toDate();
+  // }
 
   static isFutureDate(date: string | Date) {
     return moment(date).isAfter(moment());
   }
 
   static compareDates(day1: string | Date, day2: string | Date) {
-    return moment(day1).isSame(moment(day2), "day");
+    return moment(day1).isSame(moment(day2), 'day');
   }
 }
